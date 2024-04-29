378f5ce814437c22a67bdf00b444a28eb75724e3
Vgla1029 <138080760+Vgla1029@users.noreply.github.com>
2024-04-29 13:06:42 -0300

    attack.py

--- attack.py
+++ attack.py
@@ -18,24 +18,23 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-print "d    b   sSSSs   d      d s.        d ss    d ss      sSSSs     sss. "
-print "S    S  S     S  S      S  ~O       S   ~o  S   ~o   S     S  d      "
-print "S    S S         S      S   `b      S     b S     b S       S Y      
-print "S    S S         S      S sSSO      S     S S     S S       S   ss.  "
-print "S    S S    ssSb S      S    O      S     P S     P S       S      b "
-print " S   S  S     S  S      S    O      S    S  S    S   S     S       P "
-print "  °ssS   °sss°   P sSSs P    P      P ss°   P ss°     °sss°   ` ss'  "
-                                                                     
+
+
+print " _____ _____ __    _____    ____  ____      _____ "
+print "|  |  |   __|  |  |  _  |  |    \|    \ ___|   __|"
+print "|  |  |  |  |  |__|     |  |  |  |  |  | . |__   |"
+print " \___/|_____|_____|__|__|  |____/|____/|___|_____|"
+                                                    
+                                                    
 print
-print "DDoS Vgla - Made By Vgla"
-print "YouTube : https://www.youtube.com/@VglaRat"
-print "GITHUB   : https://github.com/Vgla7"
+print "Vgla DDoS - Made By Vgla"
+print "YouTube: https://www.youtube.com/@VglaRat"
+print "GitHub: https://github.com/Vgla7"
 print
-ip = raw_input("IP Do Site : ")
-port = input("Port       : ")
+ip = raw_input("IP Do Site: ")
+port = input("Porta: ")
 
 os.system("clear")
-os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(1)
 print "[=====               ] 25%"
